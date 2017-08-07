e592ba2b4b32a97aaa7dd0c1bdd45e992d3a6587
maugelves <mgelves@gmail.com>
2017-08-07 10:37:13 +0200

    Added offices list shortcode

--- inc/shortcodes/mgwpoffices.php
+++ inc/shortcodes/mgwpoffices.php
@@ -1,6 +1,11 @@
 <?php
 
-
+/**
+ * This shortcode gets all the offices and load them into
+ * a Google Map with the specific offices markers.
+ *
+ * @return string
+ */
 function fn_mgwpoffices() {
 
 	$output = "";
@@ -21,7 +26,7 @@ if( $query->have_posts() ):
 
 		$output .= sprintf( '<div class="marker" data-lat="%s" data-lng="%s">', $location['lat'], $location['lng'] );
 		$output .= "<h3>" . get_the_title() . "</h3>";
-		$output .= "<p>" . get_the_content() . "</p>";
+		$output .= get_the_content();
 		$output .= "<p><b>Dirección</b>: " . $location['address'] . "<br>";
 		if( get_field('mgwpoff_telephone') )
 			$output .= "<b>Teléfono</b>: " . get_field('mgwpoff_telephone') . "</br>";
@@ -31,6 +36,7 @@ if( $query->have_posts() ):
 
 	endwhile;
 	$output .= '</div>';
+	wp_reset_postdata();
 
 endif;
 
@@ -38,3 +44,52 @@ return $output;
 
 }
 add_shortcode( 'mgwpoffices', 'fn_mgwpoffices' );
+
+
+
+
+
+/**
+ * This shortcode gets and sets a list with all the offices.
+ *
+ * @return string
+ */
+function fn_mgwpoffices_list() {
+
+	$output = "";
+
+	$args = array(
+		'post_type'     => 'office',
+		'post_status'   => 'publish'
+	);
+
+	$query = new WP_Query( $args );
+
+
+	if( $query->have_posts() ):
+		$output .= '<ul class="mgwpoff__list">';
+		while( $query->have_posts() ): $query->the_post();
+
+			$location = get_field('mgwpoff_address');
+
+			$output .= '<li class="mgwpoff__list__item">';
+			$output .= '<h4 class="mgwpoff__list__h">' . get_the_title() . '</h4>';
+			$output .= "<div class='mgwpoff__list__b'>" . get_the_content() . "</div>";
+			$output .= "<p><b>Dirección</b>: " . $location['address'] . "<br>";
+			if( get_field('mgwpoff_telephone') )
+				$output .= "<b>Teléfono</b>: " . get_field('mgwpoff_telephone') . "</br>";
+			if( get_field('mgwpoff_fax') )
+				$output .= "<b>Fax</b>: " . get_field('mgwpoff_fax') . "</br>";
+			$output .= '</p>';
+			$output .= '</li>';
+
+		endwhile;
+		$output .= '</ul>';
+		wp_reset_postdata();
+
+	endif;
+
+	return $output;
+
+}
+add_shortcode( 'mgwpoffices-list', 'fn_mgwpoffices_list' );
